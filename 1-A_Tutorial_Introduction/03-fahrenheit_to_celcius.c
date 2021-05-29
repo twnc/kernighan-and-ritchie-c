9e7219e62e85fc4bffa6fbb0cd0faba29e9253ce
Tom <twnc@users.noreply.github.com>
2021-05-29 21:06:07 +0100

    Exercise 1-3 added heading above the table and centered table columns

--- 1-A_Tutorial_Introduction/03-fahrenheit_to_celcius.c
+++ 1-A_Tutorial_Introduction/03-fahrenheit_to_celcius.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 
 // print Fahrenheit-Celcius table for fahr = 0, 20, ..., 300; floaing point version
+// Exercise 1-3. Modify the temperature conversion program to print a heading
+// above the table
 
 main()
 {
@@ -12,9 +14,11 @@ main()
 	step = 20; // step size
 
 	fahr = lower;
+	printf(" Fahrenheit |  Celcius\n");
+	printf("------------------------\n");
 	while (fahr <= upper) {
 		celcius = (5.0/9.0) * (fahr - 32.0);
-		printf("%3.0f %6.1f\n", fahr, celcius);
+		printf("%7.0f %12.1f\n", fahr, celcius);
 		fahr = fahr + step;
 	}
 }
