016d6b27931391ff4822444a2939dd1611cbad20
Tom <twnc@users.noreply.github.com>
2021-05-29 20:58:19 +0100

    Exercise 1-3 added K&R's modifications. Switched to float for more precision and formated output

--- 1-A_Tutorial_Introduction/03-fahrenheit_to_celcius.c
+++ 1-A_Tutorial_Introduction/03-fahrenheit_to_celcius.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
-// print Fahrenheit-Celcius table for fahr = 0, 20, ..., 300
+// print Fahrenheit-Celcius table for fahr = 0, 20, ..., 300; floaing point version
 
 main()
 {
-	int fahr, celcius;
+	float fahr, celcius;
 	int lower, upper, step;
 
 	lower = 0; // lower limit of temperature table
@@ -13,10 +13,8 @@ main()
 
 	fahr = lower;
 	while (fahr <= upper) {
-		celcius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celcius);
+		celcius = (5.0/9.0) * (fahr - 32.0);
+		printf("%3.0f %6.1f\n", fahr, celcius);
 		fahr = fahr + step;
 	}
-
-	return 0;
 }
