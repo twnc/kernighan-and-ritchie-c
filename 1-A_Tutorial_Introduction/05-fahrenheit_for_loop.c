a0cf67e08993f9b8d286c2eac9567154f209fa15
Tom <twnc@users.noreply.github.com>
2021-06-01 12:16:07 +0100

    Exercise 1-5: replaced magic numbers with symbolic constants

--- 1-A_Tutorial_Introduction/05-fahrenheit_for_loop.c
+++ 1-A_Tutorial_Introduction/05-fahrenheit_for_loop.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 
+#define LOWER 0		// lower limit of table
+#define UPPER 300	// upper limit
+#define STEP 20		// step sizes
+
 // print Fahrenheit-Celcius table
 // Exercise 1-5. Modify the temperature conversion program to print the table in
 // reverse order, that is, from 300 degrees to 0.
@@ -7,6 +11,6 @@ int main()
 {
 	int fahr;
 
-	for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+	for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
 		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr - 32));
 }
