309be14fc829de0252b4b2e8b77fdfbd7813ebab
Tom <twnc@users.noreply.github.com>
2021-06-01 17:27:27 +0100

    Exercise 1-9: modified solution to remove 'else' as it wasn't taught yet

--- 1-A_Tutorial_Introduction/09-remove_multiple_blanks.c
+++ 1-A_Tutorial_Introduction/09-remove_multiple_blanks.c
@@ -3,18 +3,22 @@
 
 #include<stdio.h>
 
+// As we haven't learnt else yet, I used a boolean style int as we saw in 1-7
 int main()
 {
-	int c;
+	int c, c_is_blank;
 
 	c = getchar();
 	while (c != EOF) {
 		putchar(c);
+		c_is_blank = (c == ' ');
 		// If c was a space, get characters until c is no longer a space
-		if (c == ' ') {
+		if (c_is_blank == 1) {
 			while (c == ' ')
 				c = getchar();
-		} else
+		}
+		// If c wasn't a space, get a new character
+		if (c_is_blank == 0)
 			c = getchar();
 	}
 }
