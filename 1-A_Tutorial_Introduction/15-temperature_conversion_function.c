c1468833e582ddef4b978035d117609dbecd9e82
Tom <twnc@users.noreply.github.com>
2021-06-05 18:45:58 +0100

    Exercise 1-15 begun with K&R example code

---
+++ 1-A_Tutorial_Introduction/15-temperature_conversion_function.c
@@ -0,0 +1,21 @@
+// Exercise 1-15. Rewrite the temperature conversion program of Section 1.2 to
+// use a function for conversion.
+
+#include <stdio.h>
+
+int main()
+{
+	float fahr, celcius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+	while (fahr <= upper) {
+		celcius = (5.0/9.0) * (fahr-32.0);
+		printf("%3.0f %6.1f\n", fahr, celcius);
+		fahr = fahr + step;
+	}
+}
